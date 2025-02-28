4418cd316cff9a1c73e5254a748d905c56057335
Patterbear <benjamin.mcgregor2002@gmail.com>
2025-02-28 11:27:01 +0000

    add additional events data and update image urls

--- db/data/development-data/events.js
+++ db/data/development-data/events.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     title: "The Burton Pub Crawl",
     location: "Burton Latimer",
-    image: "https://i.postimg.cc/BZVLrcGS/pub.jpg",
+    image: "https://iili.io/3diUyAl.jpg",
     description: "A pub crawl across all the pubs in Burton Latimer!",
     date: "2025-03-27",
     time: "19:00:00"
@@ -10,7 +10,7 @@ module.exports = [
   {
     title: "LeicesTech",
     location: "Leicester",
-    image: "https://i.postimg.cc/Ls33tfsY/leics.jpg",
+    image: "https://iili.io/3diUp94.jpg",
     description: "Leicester’s annual Tech Fair featuring industry leaders and groundbreaking startups.",
     date: "2025-04-29",
     time: "10:00:00"
@@ -18,7 +18,7 @@ module.exports = [
   {
     title: "Finedon Film Festival",
     location: "Finedon",
-    image: "https://i.postimg.cc/Z5rZ8QbV/film.jpg",
+    image: "https://iili.io/3diULMX.jpg",
     description: "A festival celebrating film at Finedon cinema, with free refreshments and 20 classic movies.",
     date: "2025-05-01",
     time: "18:00:00"
@@ -26,9 +26,41 @@ module.exports = [
   {
     title: "Kettering Christmas Market",
     location: "Kettering",
-    image: "https://i.postimg.cc/bJ5Kc0YZ/christmas.jpg",
+    image: "https://iili.io/3diUDcG.jpg",
     description: "Kettering's huge annual Christmas market.",
     date: "2025-12-25",
     time: "10:00:00"
+  },
+  {
+    title: "University of Kettering Open Day",
+    location: "Kettering",
+    image: "https://iili.io/3dirmj2.jpg",
+    description: "Annual open day for the University of Kettering - open to all!",
+    date: "2025-08-19",
+    time: "10:00:00",
+  },
+  {
+    title: "Summer Coding Workshop",
+    location: "Corby",
+    image: "https://iili.io/3diUQPn.jpg",
+    description: "Come and hone your coding skills with our summer coding workshop!",
+    date: "2025-07-01",
+    time: "14:00:00"
+  },
+  {
+    title: "Karaoke Night",
+    location: "Burton Latimer",
+    image: "https://iili.io/3dsjMJI.jpg",
+    description: "Enjoy a night of song and drink at Burton's favourite karaoke night!",
+    date: "2025-05-25",
+    time: "22:00:00"
+  },
+  {
+    title: "Leicester Hackathon",
+    location: "Leicester",
+    image: "https://iili.io/3dsvxfa.jpg",
+    description: "Show off your skill and have fun by taking part in Leicester's famous yearly hackathon.",
+    date: "2025-04-14",
+    time: "19:00:00"
   }
 ];
